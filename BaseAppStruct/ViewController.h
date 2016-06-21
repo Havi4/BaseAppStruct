a3f7fca848f880e67068a629f1051046b2d6096c
Havi_li <1058695900@qq.com>
2016-06-21 14:58:19 +0800

    Initial Commit

---
+++ BaseAppStruct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseAppStruct
+//
+//  Created by Havi on 16/6/21.
+//  Copyright © 2016年 Havi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
